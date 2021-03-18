1a47b1cf1f67830127e8f1d3c1de37a1922164b8
chiranjeevcj <48284643+chiranjeevcj@users.noreply.github.com>
2021-03-18 13:17:50 +0530

    Add files via upload

---
+++ quiz/easy/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Fathometer is used to measure?",
+    answer: "Ocean depth",
+    options: [
+      "Earthquakes",
+      "Rainfall",
+      "Ocean depth",
+      "Sound intensity"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Epsom(England) is the place associated with?",
+    answer: "Horse racing",
+    options: [
+      "Snooker",
+      "Shooting",
+      " Polo",
+      "Horse racing"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Fastest Shorthand Writer was?",
+    answer: "Dr. G. D. Bist",
+    options: [
+      "Dr. G. D. Bist",
+      "J.R.D. Tata",
+      "J.M. Tagore",
+      "Khudada Khan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Golf player Vijay Singh belongs to which country?",
+    answer: "Fiji",
+    options: [
+      "USA",
+      "India",
+      " UK",
+      "Fiji"
+    ]
+  },
+    {
+    numb: 5,
+    question: "One People, One State, One leader” was the policy of?",
+    answer: "Hitler",
+    options: [
+      "Hitler",
+      "Stalin",
+      "Lenin",
+      " Mussolin"
+    ]
+  },
+ 
+];
---
+++ quiz/hard/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "DRDL stands for?",
+    answer: "Defence Research and Development Laboratory  ",
+    options: [
+      "Department of Research and Development Laboratory",
+      "Defence Research and Development Laboratory ",
+      "Differential Research and Documentation Laboratory",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Exposure to sunlight helps a person improve his health because?",
+    answer: " the ultraviolet rays convert skin oil into Vitamin D",
+    options: [
+      "the infrared light kills bacteria in the body",
+      "resistance power increases",
+      "the pigment cells in the skin get stimulated and produce a healthy tan",
+      "the ultraviolet rays convert skin oil into Vitamin D"
+    ]
+  },
+    {
+    numb: 3,
+    question: "An astronaut in outer space will observe sky as?",
+    answer: "Black",
+    options: [
+      "Black",
+      "White",
+      "Red",
+      "Green"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is known as 'the father of Indian missile technology'?",
+    answer: "Dr. A.P.J. Abdul Kalam",
+    options: [
+      "Dr. U.R. Rao",
+      "Dr. Chidambaram",
+      "Dr. Homi Bhabha",
+      "Dr. A.P.J. Abdul Kalam"
+    ]
+  },
+    {
+    numb: 5,
+    question:"Bats can fly in the dark because?",
+    answer: "they produce high pitched sounds called ultrasonics",
+    options: [
+      "they produce high pitched sounds called ultrasonics",
+      "they have a better vision in the dark",
+      "none of the above",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+];
